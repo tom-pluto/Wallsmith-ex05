0460eb5d6ac371917bd11680b722764ae7d4a376
tom-pluto <90111415+tom-pluto@users.noreply.github.com>
2021-09-05 12:41:03 -0400

    Version 1.0

--- src/main/java/Solution05.java
+++ src/main/java/Solution05.java
@@ -1,14 +1,40 @@
 /*
-What is the first number? 10
-        What is the second number? 5
-        10 + 5 = 15
-        10 - 5 = 5
-        10 * 5 = 50
-        10 / 5 = 2
+    PSEUDOCODE:
+    Define variables (numFirst, numSecond, addition, subtraction, multiplication, division)
+    Create scanner
+    Prompt for numFirst
+    Read for numFirst
+    Prompt for numSecond
+    Read for numSecond
+    Calculate addition value
+    Calculate subtraction value
+    Calculate multiplication value
+    Calculate division value
+    Print addition result, subtraction result, multiplication result, division result
 */
+
+import java.util.Scanner;
 
 public class Solution05 {
     public static void main(String[] args) {
 
+        int numFirst, numSecond, addition, subtraction, multiplication, division;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("What is the first number? ");
+        numFirst = scanner.nextInt();
+        System.out.print("What is the second number? ");
+        numSecond = scanner.nextInt();
+
+        addition = numFirst+numSecond;
+        subtraction = numFirst-numSecond;
+        multiplication = numFirst*numSecond;
+        division = numFirst/numSecond;
+
+        System.out.printf("%d + %d = %d\n%d - %d = %d\n%d * %d = %d\n%d / %d = %d",
+                numFirst, numSecond, addition,
+                numFirst, numSecond, subtraction,
+                numFirst, numSecond, multiplication,
+                numFirst, numSecond, division);
     }
 }
