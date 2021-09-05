083436130d5fb052e76eb712413e9ab2c93b7c4c
tom-pluto <90111415+tom-pluto@users.noreply.github.com>
2021-09-05 12:27:12 -0400

    Initial commit

---
+++ src/main/java/Solution05.java
@@ -0,0 +1,14 @@
+/*
+What is the first number? 10
+        What is the second number? 5
+        10 + 5 = 15
+        10 - 5 = 5
+        10 * 5 = 50
+        10 / 5 = 2
+*/
+
+public class Solution05 {
+    public static void main(String[] args) {
+
+    }
+}
